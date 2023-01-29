73b7d4de073e2c49f9102db243d4da9d161edc34
Aaron Amodt <aaron@amodttech.com>
2023-01-29 17:29:36 -0500

    html elements

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.querySelector<HTMLFormElement>("form")
+const input = document.querySelector<HTMLInputElement>("#input")
